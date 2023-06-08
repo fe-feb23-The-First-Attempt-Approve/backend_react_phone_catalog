d43fb08704a63517ab97bbdb2f83ad86c44ce45f
Pasha <pashamalyshkin2003@gmail.com>
2023-06-08 22:56:26 +0300

    added env files

--- src/utils/dbinit.ts
+++ src/utils/dbinit.ts
@@ -1,8 +1,7 @@
 /* eslint-disable max-len */
 import { Sequelize } from 'sequelize-typescript';
 
-const URI
-  = 'postgresql://PashaMalyshkin:uZTy9nvE3YjH@ep-throbbing-sunset-123571.eu-central-1.aws.neon.tech/neondb';
+const URI = process.env.DATABASE_URL;
 
 export const dbinit = () =>
   new Sequelize(URI, {
