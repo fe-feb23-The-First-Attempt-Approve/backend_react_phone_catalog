660b97ce744d3dfe3081de64abf996947e3f70f2
Mykyta Holtsov <123888109+M1k1ta@users.noreply.github.com>
2023-06-09 20:17:18 +0300

    continue developing get method (#6)
    * continue developing get method
    
    * finish to plug server

--- src/utils/dbinit.ts
+++ src/utils/dbinit.ts
@@ -3,7 +3,8 @@ import { models } from '../models';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
-const URI = process.env.DATABASE_URL || '';
+// eslint-disable-next-line max-len
+const URI = 'postgresql://PashaMalyshkin:uZTy9nvE3YjH@ep-throbbing-sunset-123571.eu-central-1.aws.neon.tech/neondb';
 
 export const dbinit = () =>
   new Sequelize(URI, {
