233c123c22c5eb72a5d3a2f8c25abb8fa1ff1287
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-06-09 20:14:02 +0300

    finish to plug server

--- build/utils/dbinit.js
+++ build/utils/dbinit.js
@@ -3,12 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbinit = void 0;
 const sequelize_typescript_1 = require("sequelize-typescript");
 const models_1 = require("../models");
-const URI = process.env.DATABASE_URL || "";
-const dbinit = () =>
-  new sequelize_typescript_1.Sequelize(URI, {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+// eslint-disable-next-line max-len
+const URI = 'postgresql://PashaMalyshkin:uZTy9nvE3YjH@ep-throbbing-sunset-123571.eu-central-1.aws.neon.tech/neondb';
+const dbinit = () => new sequelize_typescript_1.Sequelize(URI, {
     models: models_1.models,
     dialectOptions: {
-      ssl: true,
+        ssl: true,
     },
-  });
+});
 exports.dbinit = dbinit;
