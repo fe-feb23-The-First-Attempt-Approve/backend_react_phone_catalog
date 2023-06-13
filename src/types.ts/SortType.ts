78aaa235263ce6662a327efc4ae3d19176ac07d4
Vadym Dubniak <79791132+Esteban4769@users.noreply.github.com>
2023-06-13 20:58:57 +0300

    added pagination (#15)

---
+++ src/types.ts/SortType.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-shadow
+export enum SortType {
+  Name = 'name',
+  New = 'newest',
+  Old = 'oldest',
+  LowPrice = 'low-price',
+  HightPrice = 'high-price',
+}
