0906588c16699c0a74e980b0644eed8b34c2447b
Pasha <pashamalyshkin2003@gmail.com>
2023-06-14 22:33:13 +0300

    seed data to tablet table / changed jpg image to png

--- src/sync.ts
+++ src/sync.ts
@@ -1,6 +1,6 @@
-import { Accessory } from './models/Accessory';
-import { Phone } from './models/Phone';
-import { ProductDetail } from './models/ProductDetail';
+// import { Accessory } from './models/Accessory';
+// import { Phone } from './models/Phone';
+// import { ProductDetail } from './models/ProductDetail';
 import { Tablet } from './models/Tablet';
 import { dbinit } from './utils/dbinit';
 import { seedInitialData } from './utils/seedInitialData';
@@ -8,10 +8,10 @@ import { seedInitialData } from './utils/seedInitialData';
 const sync = async() => {
   dbinit();
 
-  await Phone.sync();
-  await Tablet.sync();
-  await Accessory.sync();
-  await ProductDetail.sync();
+  // await Phone.sync();
+  await Tablet.sync({ force: true });
+  // await Accessory.sync();
+  // await ProductDetail.sync();
 
   await seedInitialData();
 };
