0a0d789e695d0dfc7d2a2b3e1d59a7d08003dd12
Pasha <pashamalyshkin2003@gmail.com>
2023-06-20 23:57:36 +0300

    changed routing

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import { activate, login, register } from '../controllers/auth';
 import { catchError } from '../utils/catchError';
-import 'dotenv/config';
 
 export const router = express.Router();
 
 router.post('/registration', catchError(register));
-router.get(`https://the-first-attempt-approve.onrender.com/activation/:activationToken`, catchError(activate));
+router.get(`/activation/:activationToken`, catchError(activate));
 router.post('/login', catchError(login));
