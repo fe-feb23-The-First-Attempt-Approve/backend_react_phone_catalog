f63eebfdbc12d200d93de0e81d0175c887e131f2
Mykyta Holtsov <123888109+M1k1ta@users.noreply.github.com>
2023-06-20 18:24:09 +0300

    rebuild (#35)

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -4,6 +4,6 @@ import { getAll, getByIds, getByQuery, getHot } from '../controllers/products';
 export const router = express.Router();
 
 router.get('/', getAll);
-router.get('/ids', getByIds);
+router.post('/ids', getByIds);
 router.get('/hot', getHot);
 router.get('/search', getByQuery);
